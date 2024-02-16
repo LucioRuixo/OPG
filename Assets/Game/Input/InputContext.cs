1161ddd668de3c73e3891615382c64a23a87935b
Lucio Ruixo <54895404+LucioRuixo@users.noreply.github.com>
2024-02-15 21:06:58 -0300

    InputContext documentation

--- Assets/Game/Input/InputContext.cs
+++ Assets/Game/Input/InputContext.cs
@@ -1,7 +1,38 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace OPG.Input
 {
-    public class InputContext<Actions> : InputContextBase where Actions : new()
+    /// <summary>
+    /// Base class for all input contexts.
+    /// </summary>
+    /// <typeparam name="Actions">The actions that can be performed in this context.</typeparam>
+    public abstract class InputContext<Actions> : MonoBehaviour where Actions : Enum
     {
-		
+        private Dictionary<Actions, List<Action>> actionCallbacks = new Dictionary<Actions, List<Action>>();
+
+        /// <summary>
+        /// Subscribe a method to an action in this context, which will be called when the latter is performed.
+        /// </summary>
+        /// <param name="action">The action to subscribe to.</param>
+        /// <param name="callback">The subscribed method.</param>
+        public void SubscribeToAction(Actions action, Action callback)
+        {
+            if (!actionCallbacks.ContainsKey(action)) actionCallbacks.Add(action, new List<Action>());
+
+            actionCallbacks[action].Add(callback);
+        }
+
+        /// <summary>
+        /// Perform an action in this context, therefore calling all methods subscribed to it.
+        /// </summary>
+        /// <param name="action">The action to be performed.</param>
+        public void DoAction(Actions action)
+        {
+            if (!actionCallbacks.ContainsKey(action)) return;
+
+            foreach (Action callback in actionCallbacks[action]) callback?.Invoke();
+        }
     }
 }
